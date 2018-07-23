3a8da36b1da1d3551419e795befa75d745e48a23
marco persi <marcopersi@me.com>
2018-07-23 09:57:36 +0200

    changed bottle names Balthasar and Melchior to corresponding values

---
+++ VinoImporter/src/Preparer.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Preparer {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		FileReader aReader = new FileReader(new File("WZ-256_12032016"));
+		// reading the file
+		
+		//running through characters, drop carriage return /new line if the next line is not the next value in the sequence
+		
+		
+		
+	}
+
+}
